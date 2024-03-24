5a3f2e84923fae59440c9b3c826e88707e27024e
nichtbruno <brunopopifv@gmail.com>
2024-03-24 11:05:13 +0100

    under construction fix

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ var theme = document.getElementById("theme");
 var font_button = document.getElementById("font_button");
 
 theme.onclick = function(){
-    document.body.classList.toggle("dark_theme");
+    document.body.classList.toggle("light_theme");
 }
 font_button.onclick = function(){
     document.body.classList.toggle("alt_font");
